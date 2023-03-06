0e7995cece2087cf3771a78f1caa1d75175c6491
Wencke Liermann <wencke-liermann@web.de>
2023-03-06 18:01:44 +0900

    Remove F1 bug

--- vap/vap_model.py
+++ vap/vap_model.py
@@ -14,14 +14,14 @@ from vap.predictor import Predictor
 class VAPModel(pl.LightningModule):
     """Voice Activity Projection Model.
 
-    Based on ...
+    Based on https://arxiv.org/pdf/2205.09812.pdf
 
     The model processes acoustic input features made up
     of a combined speech waveform and the activity status of
     each speaker at time point t, together with a longer
     history of their relative participation so far.
 
-    For more details on training methods, see:
+    For more details on class methods, see:
     https://pytorch-lightning.readthedocs.io/en/
     stable/common/lightning_module.html
 
@@ -46,6 +46,48 @@ class VAPModel(pl.LightningModule):
     def forward(self, x):
         return self.predictor(self.encoder(x))
 
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(
+            self.parameters(),
+            lr=self.confg["optimizer"]["learning_rate"],
+            betas=self.confg["optimizer"]["betas"],
+            weight_decay=self.confg["optimizer"]["weight_decay"]
+        )
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=opt,
+            T_max=self.confg["optimizer"]["lr_scheduler_tmax"]
+        )
+        return {
+            "optimizer": opt,
+            "lr_scheduler": {
+                "scheduler": lr_scheduler,
+                "interval": self.confg["optimizer"]["lr_scheduler_interval"],
+                "frequency": self.confg["optimizer"]["lr_scheduler_freq"]
+            },
+        }
+
+    def _shared_step(self, batch, batch_idx):
+        labels = self.predictor.classification_head.get_gold_label(
+            batch["labels"]
+        )
+        out = self(batch)
+
+        if self.confg["class_weight"]:
+            mw = self.confg["min_weight"]
+            s = ((1-mw)/self.class_dist.max())*(self.class_dist)
+            loss = nn.functional.cross_entropy(
+                out, labels, weight=(1-s)
+            )
+            if self.current_epoch == 0:
+                for l in labels.tolist():
+                    self.class_dist[l] += 1
+        else:
+            loss = nn.functional.cross_entropy(
+                out, labels
+            )
+
+        return loss, labels, out
+
     def training_step(self, batch, batch_idx):
         loss, _, _ = self._shared_step(batch, batch_idx)
         self.log("train_loss", loss, on_step=False, on_epoch=True)
@@ -58,9 +100,13 @@ class VAPModel(pl.LightningModule):
 
         return loss, gold, pred, batch
 
+    def on_train_epoch_start(self):
+        if self.current_epoch == self.confg["optimizer"]["train_encoder_epoch"]:
+            self.encoder.set_cpc_mode(freeze=False)
+
     def validation_epoch_end(self, validation_step_outputs):
-        all_pred = dict()
-        true_pred = dict()
+        all_pred = {}
+        true_pred = {}
 
         true_events = []
         pred_events = []
@@ -86,61 +132,20 @@ class VAPModel(pl.LightningModule):
                     else:
                         pred_events.append("SHIFT")
 
+        print("\n")
+        for label in all_pred:
+            print(label, all_pred[label], true_pred.get(label), sep="\t")
+        print("\n")
+
         weights = Counter(true_events)
         hold_f1, shift_f1 = f1_score(
             true_events, pred_events, labels=["HOLD", "SHIFT"], average=None
         )
         total_f1 = (
-            weights["HOLD"]*hold_f1 + weights["SHIFT"]*shift_f1/
+            (weights["HOLD"]*hold_f1 + weights["SHIFT"]*shift_f1)/
             sum(weights.values())
         )
 
         self.log("hold_f1", hold_f1)
         self.log("shift_f1", shift_f1)
         self.log("total_f1", total_f1)
-
-    def _shared_step(self, batch, batch_idx):
-        labels = self.predictor.classification_head.get_gold_label(
-            batch["labels"]
-        )
-        out = self(batch)
-
-        if self.confg["class_weight"]:
-            mw = self.confg["min_weight"]
-            s = ((1-mw)/self.class_dist.max())*(self.class_dist)
-            loss = nn.functional.cross_entropy(
-                out, labels, weight=(1-s)
-            )
-            if self.current_epoch == 0:
-                for l in labels.tolist():
-                    self.class_dist[l] += 1
-        else:
-            loss = nn.functional.cross_entropy(
-                out, labels
-            )
-
-        return loss, labels, out
-
-    def configure_optimizers(self):
-        opt = torch.optim.AdamW(
-            self.parameters(),
-            lr=self.confg["optimizer"]["learning_rate"],
-            betas=self.confg["optimizer"]["betas"],
-            weight_decay=self.confg["optimizer"]["weight_decay"]
-        )
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer=opt,
-            T_max=self.confg["optimizer"]["lr_scheduler_tmax"]
-        )
-        return {
-            "optimizer": opt,
-            "lr_scheduler": {
-                "scheduler": lr_scheduler,
-                "interval": self.confg["optimizer"]["lr_scheduler_interval"],
-                "frequency": self.confg["optimizer"]["lr_scheduler_freq"]
-            },
-        }
-
-    def on_train_epoch_start(self):
-        if self.current_epoch == self.confg["optimizer"]["train_encoder_epoch"]:
-            self.encoder.set_cpc_mode(freeze=False)
