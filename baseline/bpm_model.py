454e124a556ab691fbf76257e0d58dff97fcfc49
Wencke Liermann <wencke-liermann@web.de>
2023-04-20 21:15:29 +0900

    Add BPM baseline model

---
+++ baseline/bpm_model.py
@@ -0,0 +1,259 @@
+import pytorch_lightning as pl
+from sklearn.metrics import confusion_matrix, f1_score
+import torch
+import torch.nn as nn
+from torch.nn.functional import relu, sigmoid, softmax
+from transformers import BertModel
+
+import argparse
+import os 
+
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    EarlyStopping,
+    LearningRateMonitor,
+)
+from torch.utils.data import DataLoader
+from bpm_utils import BCDataset
+
+
+LSTM_DIM = 100 #TODO: find the appropriate size
+
+
+class BPM_MT(pl.LightningModule):
+    def __init__(self, lm_name="bert-base-uncased"):
+        super(BPM_MT, self).__init__()
+
+        # Model Architecture
+        # Transformer
+        self.lm = BertModel.from_pretrained(
+            lm_name,
+            attention_probs_dropout_prob=0.3,
+            hidden_dropout_prob=0.3
+        )
+        # Additional Parameter
+        self.lstm = nn.LSTM(
+            13, LSTM_DIM, bidirectional=True, batch_first=True
+        )
+
+        self.fc1 = nn.Linear(768 + LSTM_DIM, 128)
+        self.fc2 = nn.Linear(128, 3)
+        self.fc3 = nn.Linear(768, 64)
+        self.fc4 = nn.Linear(64, 3)
+
+        self.label_to_idx = {
+            "NO-BC": 0,
+            "CONTINUER": 1,
+            "ASSESSMENT": 2
+        }
+
+        # this property activates manual optimization.
+        self.automatic_optimization = False
+
+    def configure_optimizers(self):
+        """Define algorithms and schedulers used in optimization."""
+        # optimization uses SGD for transformer params and Adam for other params
+        transf_opt = torch.optim.SGD(self.lm.parameters(), lr=0.0005)
+        other_opt = torch.optim.Adam([
+            *self.lstm.parameters(),
+            *self.fc1.parameters(),
+            *self.fc2.parameters(),
+            *self.fc3.parameters(),
+            *self.fc4.parameters()
+        ], lr=0.0005)
+
+        return transf_opt, other_opt
+
+    def forward(
+        self,
+        text_input_ids,
+        text_attention_mask,
+        acoustic_input,
+        train=False
+    ):
+        """Define computation performed at model call."""
+        lm_output = self.lm(
+            input_ids=text_input_ids, attention_mask=text_attention_mask
+        )[0]
+
+        # encoded vector of the CLS token is used as the lexical representation
+        cls_token_encoding = lm_output[:,0,:]
+
+        ## Main Task
+
+        # feed mfcc feature vectors into LSTM layers for acoustic representation
+        outputs, ((forward, backward), final_cell) = self.lstm(acoustic_input)
+        # add the final hidden states from the forward and backward pass
+        acoustic_encoding = torch.add(forward, backward)
+
+        # acoustic and lexical hidden representations are concatenated
+        combined_encoding = torch.cat((acoustic_encoding, cls_token_encoding), 1)
+
+        # feed into fully connected FC128 layer with final softmax normalisation
+        # outputing a probability distribution across the 4 BC categories
+        hidden_emb = relu(self.fc1(combined_encoding))
+        main_output = torch.softmax(self.fc2(hidden_emb), 1)
+
+        ## Sub-Task
+
+        sub_output = None
+        if train:
+            hidden_emb = relu(self.fc3(cls_token_encoding))
+            # predicted normalized match count per sentiment category
+            sub_output = torch.sigmoid(self.fc4(hidden_emb))
+
+        return main_output, sub_output
+
+    def training_step(self, batch, batch_idx):
+        """Compute and return training loss."""
+        main_criterion = nn.CrossEntropyLoss()
+        sub_criterion = nn.BCELoss(reduction='mean')
+        
+        # compute loss
+        main_output, sub_output = self.forward(
+            batch["input_ids"], batch["attention_mask"],
+            batch["acoustic_input"], train=True
+        )
+        main_labels = torch.tensor(
+            [self.label_to_idx[l] for l in batch["main_labels"]],
+            device=main_output.device
+        )
+        main_loss, sub_loss = (
+            (1-0.1)*main_criterion(main_output, main_labels)
+            , 0.1*sub_criterion(sub_output, batch["sub_labels"])
+        )
+        total_loss = main_loss + sub_loss
+
+        transf_opt, other_opt = self.optimizers()
+        
+        # optimize through back pass
+        transf_opt.zero_grad()
+        other_opt.zero_grad()
+        self.manual_backward(total_loss)
+        transf_opt.step()
+        other_opt.step()
+
+        # record loss values
+        self.log("main_loss", main_loss, on_step=False, on_epoch=True)
+        self.log("sub_loss", sub_loss, on_step=False, on_epoch=True)
+        self.log("total_loss", total_loss, on_step=False, on_epoch=True)
+
+        return total_loss
+
+    def validation_step(self, batch, batch_idx):
+        """Compute and return validation loss."""
+        main_criterion = nn.CrossEntropyLoss()
+
+        # compute loss
+        main_output, _ = self.forward(
+            batch["input_ids"], batch["attention_mask"],
+            batch["acoustic_input"], train=False
+        )
+        main_labels = torch.tensor(
+            [self.label_to_idx[l] for l in batch["main_labels"]],
+            device=main_output.device
+        )
+        main_loss = main_criterion(main_output, main_labels)
+        self.log("val_loss", main_loss, on_step=False, on_epoch=True)
+        
+        return main_labels, torch.argmax(main_output, dim=1)
+
+    def validation_epoch_end(self, validation_step_out):
+        """Call at validation time in the very end of the epoch."""
+        gold = []
+        pred = []
+
+        for g, p in validation_step_out:
+            gold.extend(g.tolist())
+            pred.extend(p.tolist())
+
+        conf_mtrx = confusion_matrix(
+            # ["NO-BC", "CONTINUER", "ASSESSMENT"]
+            gold, pred, labels=[0, 1, 2]
+        )
+        print(conf_mtrx)
+
+        f1 = f1_score(gold, pred, average="weighted")
+        self.log("f1", f1)
+
+        return conf_mtrx
+
+
+if __name__ == "__main__":
+    pl.seed_everything(1)
+
+    # configure commandline interface
+    parser = argparse.ArgumentParser(
+        description="Interface to train the BPM_MT (2021) Model."
+    )
+    parser.add_argument(
+        "--load", metavar="CKPT_PATH", default=None,
+        help="path to existing model state"
+    )
+    parser.add_argument(
+        "--save", metavar="CKPT_NAME", default="checkpoint",
+        help="name to save model state under"
+    )
+
+    args = parser.parse_args()
+    path = os.path.dirname(os.path.dirname(__file__))
+
+    # prepare data
+    print("Load data ...")
+    train_dataset = BCDataset(
+        os.path.join(path, "data", "swb", "conversations.train"),
+        os.path.join(
+            path, "data", "swb",
+            "utterance_is_backchannel_with_sentiment_with_context.csv"
+        ),
+        os.path.join(path, "data", "swb", "swb_audios")
+    )
+    train_data = DataLoader(train_dataset, batch_size=16)
+    valid_dataset = BCDataset(
+        os.path.join(path, "data", "swb", "conversations.valid"),
+        os.path.join(
+            path, "data", "swb",
+            "utterance_is_backchannel_with_sentiment_with_context.csv"
+        ),
+        os.path.join(path, "data", "swb", "swb_audios")
+    )
+    valid_data = DataLoader(valid_dataset, batch_size=16)
+
+    # prepare model
+    print("Build model ...")
+    model = BPM_MT("bert-base-uncased")
+    print("COMPLETE")
+
+    # configure training procedure
+    callbacks = [
+        ModelCheckpoint(
+            monitor="val_loss",
+            mode="max",
+            dirpath=os.path.join(
+                path, "data", "model_checkpoints"
+            ),
+            filename=args.save + "-{epoch}-{step}"
+        ),
+        EarlyStopping(
+            monitor="val_loss",
+            mode="max",
+            patience=5,
+            strict=True,
+            verbose=False,
+        ),
+        LearningRateMonitor()
+    ]
+    trainer = pl.Trainer(
+        callbacks=callbacks,
+        deterministic=True,
+        max_epochs=60,
+        accelerator="gpu"
+    )
+
+    # actual training
+    trainer.fit(
+        model,
+        ckpt_path=args.load,
+        train_dataloaders=train_data,
+        val_dataloaders=valid_data
+    )
---
+++ baseline/bpm_utils.py
@@ -0,0 +1,171 @@
+"""bpm_utils.py - BPM Baseline Dataloader"""
+import os
+import random
+
+import librosa
+import torch
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from transformers import BertTokenizer
+
+
+class BCDataset(torch.utils.data.IterableDataset):
+    # text tokenizer for bc model preprocessing
+    tokenizer = BertTokenizer.from_pretrained(
+        "bert-base-uncased"
+    )
+
+    # sentiment soft label extractor
+    sent_tokenizer = AutoTokenizer.from_pretrained(
+        "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    )
+    sent_model = AutoModelForSequenceClassification.from_pretrained(
+        "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    ).to("cuda:0" if torch.cuda.is_available() else "cpu")
+    for p in sent_model.parameters():
+        # this model should be permanently frozen
+        p.requires_grad_(False)
+
+    def __init__(self, split_info, text_path, audio_path):
+        """Dataset loader.
+
+        Args:
+            split_info (str/list):
+                Path to an utf8 encoded txt-file holding one
+                conversation identifer per line or
+                collection of relevant conversation identifiers.
+            text_path (str):
+                Path to a utf8-encoded file holding sample info.
+                The following tab-separated info is expected:
+                    + dialogID with BC_speaker
+                    + timestamp
+                    + BC_type
+                    + utterance history
+            audio_path (str):
+                Path to a folder holding audio files.
+                One audio file per dialogue.
+
+        """
+        super().__init__()
+
+        # data sources
+        if isinstance(split_info, str):
+            with open(split_info, 'r', encoding="utf-8") as data:
+                self.split_info = data.read().strip().split("\n")
+        else:
+            self.split_info = split_info
+        self.text_path = text_path
+        self.audio_path = audio_path
+
+    @classmethod
+    def tokenize(cls, utt):
+        """Prepare raw text data to be suitable model input.
+
+        Args:
+            utt (str/list): Text instance(s).
+
+        Returns:
+            dict: dict_keys(['input_ids', 'attention_mask'])
+                Each value of shape (N_Inputs, 128), where 128
+                is the max length of the model and longer
+                strings are truncated.
+
+        """
+        return cls.tokenizer(
+            utt, return_tensors='pt',
+            padding="max_length", max_length=128, truncation=False
+        )
+
+    @classmethod
+    def get_sentiment_score(cls, utt):
+        """Assign soft sentiment label to text utterance.
+
+        Args:
+            utt (str/list): Text instance(s).
+
+        Returns:
+            torch.tensor: (N, 3)
+                N: Number of text instances
+
+        """
+        encoded_input = cls.sent_tokenizer(
+            utt, return_tensors='pt',
+            padding="max_length", max_length=128, truncation=False
+        ).to("cuda:0" if torch.cuda.is_available() else "cpu")
+        output = cls.sent_model(**encoded_input)
+
+        return output[0].softmax(1).squeeze(0)
+
+    @staticmethod
+    def get_mfcc_feature(audio_path, end, window_len=25, step_len=10):
+        """Extract Mel-frequency Cepstrum Coefficients.
+
+        Args:
+            audio_path (str): Path to audio file.
+            end (int): The end of the extraction interval in s.
+            window_len (int): Pooled audio signal per MFCC vector in ms.
+            step_len(int): Step width between consecutive windows.
+
+        Returns:
+            tuple: (N, F)
+              N: Number of frames (depends on audio-, window-, step-len)
+              F: Number of MFCC coefficients (= 13)
+
+        """
+        audio, sr = librosa.load(
+            audio_path, offset=end-1.5, duration=1.5
+        )
+        mfcc = librosa.feature.mfcc(
+            y=audio, sr=sr, n_mfcc=13, center=False,
+            n_fft=sr//1000*window_len, hop_length=sr//1000*step_len
+        )
+        return torch.from_numpy(mfcc).permute(1, 0)
+
+    def _preprocess(self, line):
+        """Prepare relevant input features."""
+        # info, time_stamp, bc_type, *hist = line.rstrip("\n").split("\t")
+        (
+            info, time_stamp, bc_type, neg_sent, neut_sent, pos_sent, *hist
+        ) = line.rstrip("\n").split("\t")
+        diag_id, bc_speaker = info[2:-1], info[-1]
+
+        # feature preprocessing
+        # sent_feat = self.get_sentiment_score(hist[0])
+        sent_feat = torch.tensor(
+            [float(neg_sent), float(neut_sent), float(pos_sent)]
+        )
+        text_feat = self.tokenize(hist[0])
+        audio_feat = self.get_mfcc_feature(
+            os.path.join(self.audio_path, f"sw0{diag_id}.sph"),
+            float(time_stamp)
+        )
+
+        return {
+            "input_ids": text_feat["input_ids"].squeeze(0),
+            "attention_mask": text_feat["attention_mask"].squeeze(0),
+            "acoustic_input": audio_feat,
+            "main_labels": bc_type,
+            "sub_labels": sent_feat
+        }
+
+    def _load_bc_samples(self):
+        """Create pseudo-random data samples."""
+        with open(self.text_path, "r", encoding="utf-8") as file:
+            # memory for pseudo-random sampling
+            memory = []
+            for i, line in enumerate(file, 1):
+                if len(memory) % 500 == 0:
+                    random.shuffle(memory)
+                    for sample in memory:
+                        # create sample on demand
+                        yield self._preprocess(sample)
+                    memory = []
+                if line[2:6] in self.split_info:
+                    memory.append(line)
+
+            random.shuffle(memory)
+            for sample in memory:
+                yield self._preprocess(sample)
+
+    def __iter__(self):
+        return self._load_bc_samples()
